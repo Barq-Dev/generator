cd02652a6f3fa3c87f3ea0d8213938881ce379a4
Trias Bratayudhana <triasbrata@gmail.com>
2018-10-03 00:55:20 +0800

    U: add backslash

--- src/Providers/GeneratorServiceProvider.php
+++ src/Providers/GeneratorServiceProvider.php
@@ -8,7 +8,7 @@ class GeneratorServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		$this->publishes([
-			__DIR__ . '../config/generator.php' => config_path('generator.php'),
+			__DIR__ . '/../config/generator.php' => config_path('generator.php'),
 		]);
 	}
 }

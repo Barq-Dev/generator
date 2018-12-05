631e98d998687676591e62b7ee9eb7910345aaf6
Trias Bratayudhana M <triasbrata@gmail.com>
2018-12-05 15:00:34 +0800

    update generator

--- src/Providers/GeneratorServiceProvider.php
+++ src/Providers/GeneratorServiceProvider.php
@@ -2,11 +2,20 @@
 namespace Generator\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Filesystem\Filesystem;
 
 class GeneratorServiceProvider extends ServiceProvider
 {
+	public function __construct(Filesystem $fs)
+	{
+		$this->fs = $fs;
+	}
+
 	public function boot()
 	{
+		if ($this->fs->exist(config_path('generator.php'))) {
+			$this->fs->delete(config_path('generator.php'));
+		}
 		$this->publishes([
 			__DIR__ . '/../config/generator.php' => config_path('generator.php'),
 		]);
